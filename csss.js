7f85097c47acc978ae78f13430d9e91140ba4827
amircs2004 <saidimahdiamir@gmail.com>
2024-08-04 13:43:52 +0100

    java script

---
+++ csss.js
@@ -0,0 +1,14 @@
+consti = 3.14 ;  
+const r = 8.31 ; 
+const h = 4 ; 
+volume ();
+
+function volume( r , h ) {
+     return pi * r * r * h ;
+}
+console.log(volume(r,h));
+
+for (let i = 0; i < 6; i++) {
+    console.log(array[i]+".ing");
+    let array = ["aa" , "bb" , "cc" , "dd" ,"ee" , "ff"];
+}
